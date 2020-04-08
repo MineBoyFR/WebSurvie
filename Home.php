7fe80cbc22337e1c0e5325acf39008f9460e16c2
Yao <mine.boy230@gmail.com>
2020-04-08 16:49:39 +0200

    V0.0.7
    Add Algo

--- Home.php
+++ Home.php
@@ -25,13 +25,25 @@
     </aside>
     <article>
         <div>
-            <form>
-                <input type="text" name="pseudo" id="pseudo" placeholder="Votre Pseudo">
+            <form method="post">
+                <input type="text" name="pseudo" id="pseudo" placeholder="Votre Pseudo" required>
                 <input type="text" name="age" id="age" placeholder="Votre age">
-                <input type="text" name="discord" id="discord" placeholder="Votre Discord">
+                <input type="text" name="discord" id="discord" placeholder="Votre Discord" required>
                 <input type="text" name="motivation" id="motivation" placeholder="Votre motivation">
                 <input type="submit" name="envoie" id="envoie">
             </form>
+            <?php
+            if (isset($_POST['envoie'])) {
+                $pseudo = $_POST['pseudo'];
+                $discord = $_POST['discord'];
+
+                if (!empty($pseudo) && !empty($discord)) {
+                    echo "Votre Pseudo " . $_POST['pseudo'] . "<br/>";
+                    echo "Votre Age " . $_POST['age'] . "<br/>";
+                    echo "Votre Discord " . $_POST['discord'] . "<br/>";
+                }
+            }
+            ?>
         </div>
     </article>
 </section>
