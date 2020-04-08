e8dc1747c5e07b83c4cea0aad903f3c235393186
Yao <mine.boy230@gmail.com>
2020-04-08 14:44:04 +0200

    V0.0.1
    Mise en place du site web

---
+++ Home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Page home</title>
+    <link rel="stylesheet" href="#">
+</head>
+<body>
+<header>
+    <nav class="postitre">
+        <h1 id="titre1">Titre</h1>
+        <h3 id="titre2">SousTitre</h3>
+    </nav>
+    <div id="gif">
+        <!--Gif Fond page Moitier 1-->
+    </div>
+    <div id="bloc2">
+        <!--Gif Fond page Moitier 2-->
+    </div>
+</header>
+</body>
+
+<?php
+
+?>
+
+
